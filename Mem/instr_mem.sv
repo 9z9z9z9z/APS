a3490e1b1a1d358abe9aaaeaa6602c1175ff89d0
9z9z9z9z <72688414+9z9z9z9z@users.noreply.github.com>
2024-03-21 14:39:44 +0300

    upload Memory files

---
+++ Mem/instr_mem.sv
@@ -0,0 +1,15 @@
+module instr_mem(
+    input  logic [31:0] addr_i, 
+    
+    output logic [31:0] read_data_o
+    );
+    
+    logic [31:0] ROM [0:1023];
+    
+    initial begin
+        $readmemh("program.mem", ROM);
+    end
+    
+    assign read_data_o = ROM[addr_i[11:2]];
+        
+endmodule
